8f62f04a265150922d409c1b2c975dcbf61ad59d
befierce <enterbhatt123@gmail.com>
2023-08-28 15:48:11 +0530

    fetching related products

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,8 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  req.user.createProduct({title: title,
+  req.user.createProduct({
+    title: title,
     price: price,
     imageUrl: imageUrl,
     description: description
@@ -34,18 +35,20 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId)
+  req.user.getProducts({ where: { id: prodId } });
+  // Product.findById(prodId)
     .then(product => {
-      if (!product) {
-        return res.redirect('/');
-      }
-      res.render('admin/edit-product', {
-        pageTitle: 'Edit Product',
-        path: '/admin/edit-product',
-        editing: editMode,
-        product: product
-      });
-    })
+    const product = products[0];
+    if (!product) {
+      return res.redirect('/');
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product
+    });
+  })
     .catch(err => console.log(err));
 };
 
@@ -71,7 +74,8 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  req.user
+    .getProducts() 
     .then(products => {
       res.render('admin/products', {
         prods: products,
